34a5addf738abd152ee15af9e5f8cbf5cd354a11
jsp8680 <97013156+jsp8680@users.noreply.github.com>
2023-04-06 13:05:48 -0600

    Change for messages.js

---
+++ app.js
@@ -0,0 +1,5 @@
+const messages = require('./messages.js');
+
+for(let i = 0; i < messages.length; i++) {
+  console.log(messages[i]);
+}
---
+++ messages.js
@@ -0,0 +1,25 @@
+
+// create a messages array
+const messages = [ "We cannot solve problems with the kind of thinking we employed when we came up with them.” — Albert Einstein",
+"Learn as if you will live forever, live like you will die tomorrow.” — Mahatma Gandhi",
+"Stay away from those people who try to disparage your ambitions. Small minds will always do that, but great minds will give you a feeling that you can become great too.” — Mark Twain",
+"When you give joy to other people, you get more joy in return. You should give a good thought to happiness that you can give out.”— Eleanor Roosevelt",
+"When you change your thoughts, remember to also change your world.”—Norman Vincent Peale",
+"It is only when we take chances, when our lives improve. The initial and the most difficult risk that we need to take is to become honest. —Walter Anderson",
+"Nature has given us all the pieces required to achieve exceptional wellness and health, but has left it to us to put these pieces together.”—Diane McLaren",
+"The only way to do great work is to love what you do. If you haven’t found it yet, keep looking. Don’t settle. As with all matters of the heart, you’ll know when you find it.” — Steve Jobs",
+"Life is a succession of lessons which must be lived to be understood.” — Ralph Waldo Emerson",
+"Success is not final; failure is not fatal: It is the courage to continue that counts. — Winston S. Churchill",
+"It is better to fail in originality than to succeed in imitation. — Herman Melville",
+"The road to success and the road to failure are almost exactly the same. — Colin R. Davis",
+"Success usually comes to those who are too busy to be looking for it. — Henry David Thoreau",
+"Develop success from failures. Discouragement and failure are two of the surest stepping stones to success.” —Dale Carnegie",
+"Nothing in the world can take the place of Persistence. Talent will not; nothing is more common than unsuccessful men with talent. Genius will not; unrewarded genius is almost a proverb. Education will not; the world is full of educated derelicts. The slogan 'Press On' has solved and always will solve the problems of the human race. —Calvin Coolidge",
+"There are three ways to ultimate success: The first way is to be kind. The second way is to be kind. The third way is to be kind. —Mister Rogers",
+"Success is not the key to happiness. Happiness is the key to success. If you love what you are doing, you will be successful.” —Albert Schweitzer",
+"Success is peace of mind, which is a direct result of self-satisfaction in knowing you made the effort to become the best of which you are capable.” —John Wooden",
+"I never dreamed about success. I worked for it.” —Estée Lauder",
+];
+
+// export the array
+module.exports = messages;
